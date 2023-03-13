2461f9cbd934084d5188dc0c29713810d44791f6
NicolasIUT <ferrayen@3il.fr>
2023-03-13 21:29:05 +0100

    fix: correction de la classe Champ

--- src/main/java/com/stardew/stardewvalley/Champ.java
+++ src/main/java/com/stardew/stardewvalley/Champ.java
@@ -1,10 +1,34 @@
 package com.stardew.stardewvalley;
 
+/**
+ * Classe Champ héritant d'ObjetInteractif
+ *
+ * @since le 13/03/2023
+ * @version le 13/03/2023
+ *
+ * @author Nicolas Ferrayé - 3iL
+ */
 public class Champ extends ObjetInteractif {
 
+    private boolean estCultive; // Si le champ est cultivé ou non
 
-    public Champ(int posX, int posY) {
-        super(posX, posY);
+    public Champ(int pfPosX, int pfPosY, boolean pfEstCultive) {
+        super(pfPosX, pfPosY, true);
+        this.estCultive = pfEstCultive;
+    }
+
+    /**
+     * @return true si le champ est cultivé, sinon false
+     */
+    public boolean getEstCultive() {
+        return this.estCultive;
+    }
+
+    /**
+     * @param pfEstCultive si le champ est cultivé ou non
+     */
+    public void setEstCultive(boolean pfEstCultive) {
+        this.estCultive = pfEstCultive;
     }
 
 }
