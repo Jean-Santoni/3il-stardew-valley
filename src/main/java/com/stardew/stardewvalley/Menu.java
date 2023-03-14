51299e96acc383c2a0c6575982194a488cf5e369
clemse32 <117434193+clemse32@users.noreply.github.com>
2023-03-14 09:05:44 +0100

    Rajout classe Menu + plein écran

---
+++ src/main/java/com/stardew/stardewvalley/Menu.java
@@ -0,0 +1,21 @@
+package com.stardew.stardewvalley;
+
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+public class Menu extends Pane{
+
+    public Menu() {
+        super();
+        this.setBackground(new Background(new BackgroundImage(new Image("file:Images/Fond_menu.png"),
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT)));
+    }
+    public Menu(Node... arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+}
