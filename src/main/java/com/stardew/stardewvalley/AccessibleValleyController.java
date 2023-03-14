601db4a1cd9aad3d2842cf22763ca3bc049948f9
Jean-Santoni <santonigs.2a@gmail.com>
2023-03-14 10:54:07 +0100

    modification grille de jeu et Carte

--- src/main/java/com/stardew/stardewvalley/AccessibleValleyController.java
+++ src/main/java/com/stardew/stardewvalley/AccessibleValleyController.java
@@ -2,15 +2,28 @@ package com.stardew.stardewvalley;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+
+import java.io.FileNotFoundException;
 
 public class AccessibleValleyController {
+
 
     @FXML
     BorderPane borderPanel;
 
     @FXML
-    private void initialize() {
-        this.borderPanel.setCenter(new Menu());
+    private void initialize() throws FileNotFoundException {
+        Carte carte = new Carte();
+        carte.add(new Text("sidufhsdifh"),1,2);
+        //carte.add(new Champ(5,5).getImage(),1,3);
+
+
+        // Pierre p = new Pierre(50,20);
+        this.borderPanel.setCenter(carte);
+      //  this.borderPanel.setTop(p.getImage());
+
     }
 }
--- src/main/java/com/stardew/stardewvalley/Carte.java
+++ src/main/java/com/stardew/stardewvalley/Carte.java
@@ -2,17 +2,16 @@ package com.stardew.stardewvalley;
 
 import javafx.scene.Node;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 
-public class Carte extends Pane {
+public class Carte extends GridPane {
+    private final int MAX_WIDTH = 20;
+    private final int MAX_HEIGHT = 50;
 
     public Carte() {
         super();
+        initialiserCarte();
         this.setBackground(new Background(new BackgroundImage(new Image("file:Images/Map.jpg"),
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
@@ -20,9 +19,43 @@ public class Carte extends Pane {
                 BackgroundSize.DEFAULT)));
     }
 
-    public Carte(Node... arg0) {
-        super(arg0);
-        // TODO Auto-generated constructor stub
+    private void initialiserCarte() {
+        this.getColumnConstraints().clear();
+        this.getRowConstraints().clear();
+        for (int c = 0; c <= MAX_WIDTH; c++) {
+            ColumnConstraints column = new ColumnConstraints();
+            column.setPercentWidth(100.0 / 2);
+            this.getColumnConstraints().add(column);
+        }
+        for (int r = 0; r <= MAX_HEIGHT; r++) {
+            RowConstraints row = new RowConstraints();
+            row.setPercentHeight(100.0 / 3);
+            this.getRowConstraints().add(row);
+        }
+        this.setGridLinesVisible(true);
+       /* int collIndex = 0;
+        int rowIndex = 0;
+        int listeIndex = 0;
+        for (collIndex = 0; collIndex <= 2; collIndex++) {
+            for (rowIndex = 0; rowIndex <= 3; rowIndex++) {
+                ImageView n = new ImageView();
+                if(ListeCarte.get(listeIndex).isClique()==false) {
+                    n.setImage(ListeCarte.get(listeIndex).getDos());
+                }else if(ListeCarte.get(listeIndex).isValide() && ListeCarte.get(listeIndex).isClique() ){
+                    n.setImage(ListeCarte.get(listeIndex).getValide());
+                }else {
+                    n.setImage(ListeCarte.get(listeIndex).getFace());
+                }
+                n.setFitWidth(100.0);
+                n.setFitHeight(100.0);
+                Carte carteTampon = ListeCarte.get(listeIndex);
+                n.setOnMouseClicked(event ->CliqueCase(carteTampon) );
+                this.add(n, collIndex, rowIndex);
+                listeIndex++;
+            }
+        }
+        listeIndex=0;*/
     }
+
 
 }
