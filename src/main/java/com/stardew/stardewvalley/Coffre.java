5615a38e77713305bea68a215caa43e0a58df218
clemse32 <117434193+clemse32@users.noreply.github.com>
2023-03-22 11:46:34 +0100

    Choix du Coffre

--- src/main/java/com/stardew/stardewvalley/Coffre.java
+++ src/main/java/com/stardew/stardewvalley/Coffre.java
@@ -1,8 +1,12 @@
 package com.stardew.stardewvalley;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceDialog;
 import javafx.scene.image.Image;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Coffre extends ObjetInteractif {
     public Coffre(int pfPosX, int pfPosY) {
@@ -13,8 +17,16 @@ public class Coffre extends ObjetInteractif {
     @Override
     public void interraction() throws IOException {
         super.interraction();
-        setImage(null);
+        validationVendre(true);
 
     }
+    public void validationVendre(boolean b) {
+        String[] choices = {"Houblon", "Blé", "Pattate", "Tomate","Maïs"};
+        ChoiceDialog<String> cDial = new ChoiceDialog<>(choices[2], choices);
+        cDial.setTitle("Marché");
+        cDial.setHeaderText("Que voulez-vous vendre ?");
+        Optional<String> selection = cDial.showAndWait();
+        selection.ifPresent(str -> System.out.println("Selection:" + str));
 
+    }
 }
